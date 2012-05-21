e2b87c3b4de53884d1b2f21839fa0eee36955271
Chris Heydrick <cheydrick@gmail.com>
2012-05-21 16:39:26 -0500

    Initial commit

---
+++ GTKPixBufTest.c
@@ -0,0 +1,17 @@
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main (int argc, char *argv[])
+{
+  GtkWidget *window;
+
+  gtk_init(&argc, &argv);
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_widget_show(window);
+
+  gtk_main();
+
+  return 0;
+}
